ee204e6eabb12120bb8526aafb2b74659ae81f7b
Tugberk Ugurlu <tugberk@outlook.com>
2015-11-02 23:13:22 +0000

    first intiration on ModernShopping.Auth

---
+++ Auth/ModernShopping.Auth.Host.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin.Hosting;
+
+namespace ModernShopping.Auth.Host.Console
+{
+    using Console = System.Console;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            const string url = "https://localhost:44333/core";
+            Console.Title = "IdentityServer3 SelfHost";
+            
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine("\n\nServer listening at {0}. Press enter to stop", url);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Auth/src/ModernShopping.Auth.Host/Startup.cs
@@ -0,0 +1,31 @@
+﻿using IdentityServer3.Core.Configuration;
+using ModernShopping.Auth.Host.Config;
+using Owin;
+using Serilog;
+
+namespace ModernShopping.Auth.Host
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Trace(outputTemplate: "{Timestamp} [{Level}] ({Name}){NewLine} {Message}{NewLine}{Exception}")
+                .CreateLogger();
+
+            var factory = new IdentityServerServiceFactory()
+                        .UseInMemoryUsers(Users.Get())
+                        .UseInMemoryClients(Clients.Get())
+                        .UseInMemoryScopes(Scopes.Get());
+
+            var options = new IdentityServerOptions
+            {
+                SigningCertificate = Certificate.Get(),
+                Factory = factory,
+                SiteName = "ModernShopping"
+            };
+
+            appBuilder.UseIdentityServer(options);
+        }
+    }
+}
