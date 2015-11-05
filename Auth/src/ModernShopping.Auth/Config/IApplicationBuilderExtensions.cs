95a197911a56b4139404305f890afa5f345437bb
Tugberk Ugurlu <tugberk@outlook.com>
2015-11-05 21:33:59 +0000

    asp.net 5 auth server

---
+++ Auth/src/ModernShopping.Auth/Config/IApplicationBuilderExtensions.cs
@@ -0,0 +1,38 @@
+﻿using IdentityServer3.Core.Configuration;
+using Microsoft.AspNet.Builder;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ModernShopping.Auth.Config
+{
+    using DataProtectionProviderDelegate = Func<string[], Tuple<Func<byte[], byte[]>, Func<byte[], byte[]>>>;
+    using DataProtectionTuple = Tuple<Func<byte[], byte[]>, Func<byte[], byte[]>>;
+
+    public static class IApplicationBuilderExtensions
+    {
+        public static void UseIdentityServer(this IApplicationBuilder app, IdentityServerOptions options)
+        {
+            app.UseOwin(addToPipeline =>
+            {
+                addToPipeline(next =>
+                {
+                    var builder = new Microsoft.Owin.Builder.AppBuilder();
+                    var provider = app.ApplicationServices.GetService(typeof(Microsoft.AspNet.DataProtection.IDataProtectionProvider)) as Microsoft.AspNet.DataProtection.IDataProtectionProvider;
+
+                    builder.Properties["security.DataProtectionProvider"] = new DataProtectionProviderDelegate(purposes =>
+                    {
+                        var dataProtection = provider.CreateProtector(string.Join(",", purposes));
+                        return new DataProtectionTuple(dataProtection.Protect, dataProtection.Unprotect);
+                    });
+
+                    builder.UseIdentityServer(options);
+
+                    var appFunc = builder.Build(typeof(Func<IDictionary<string, object>, Task>)) as Func<IDictionary<string, object>, Task>;
+                    return appFunc;
+                });
+            });
+        }
+    }
+}
---
+++ Auth/src/ModernShopping.Auth/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+using ModernShopping.Auth.Config;
+using Microsoft.Dnx.Runtime;
+using System.IO;
+using IdentityServer3.Core.Configuration;
+using System.Security.Cryptography.X509Certificates;
+using ModernShopping.Auth.Host.Config;
+
+namespace ModernShopping.Auth
+{
+    public class Startup
+    {
+        private readonly IApplicationEnvironment _appEnv;
+
+        public Startup(IApplicationEnvironment appEnv)
+        {
+            _appEnv = appEnv;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDataProtection();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            // TOOD: PR this to https://github.com/IdentityServer/IdentityServer3.Samples/blob/master/source/AspNet5Host/src/IdentityServerAspNet5/Startup.cs#L21
+            var certFile = Path.Combine(_appEnv.ApplicationBasePath, "idsrv3test.pfx");
+            var idsrvOptions = new IdentityServerOptions
+            {
+                SigningCertificate = new X509Certificate2(certFile, "idsrv3test"),
+                RequireSsl = false,
+
+                AuthenticationOptions = new AuthenticationOptions
+                {
+                    EnablePostSignOutAutoRedirect = true
+                },
+
+                Factory = new IdentityServerServiceFactory()
+                    .UseInMemoryUsers(Users.Get())
+                    .UseInMemoryClients(Clients.Get())
+                    .UseInMemoryScopes(Scopes.Get())
+            };
+
+            app.UseDeveloperExceptionPage();
+            app.UseIdentityServer(idsrvOptions);
+        }
+    }
+}
