fc361fb07b10cbaf5b727f81b522b7f1e7630de1
Tugberk Ugurlu <tugberk@outlook.com>
2015-11-06 23:15:24 +0000

    started ModernShopping.Lookup.Queries

---
+++ Lookup/src/ModernShopping.Lookup.Queries/Models/Product.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ModernShopping.Lookup.Queries.Entities
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public string Description { get; set; }
+        public string PrimaryImageUrl { get; set; }
+        public BrandReference Brand { get; set; }
+
+        public Occurrence DeletedOn { get; set; }
+    }
+
+    public class Occurrence
+    {
+        public Occurrence() : this(DateTime.UtcNow)
+        {
+        }
+
+        public Occurrence(DateTime occurredOn)
+        {
+        }
+
+        public DateTime OccurredOn { get; private set; }
+    }
+
+    public class BrandReference
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+    }
+
+    public class ProductRatingSummary
+    {
+        public long TotalCount { get; set; }
+        public byte AverageRating { get; set; }
+    }
+
+    public class ProductReviewSummary
+    {
+        public long TotalCount { get; set; }
+    }
+}
---
+++ Lookup/src/ModernShopping.Lookup.Queries/ProductByIdQuery.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Framework.Logging;
+using ModernShopping.Lookup.Queries.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModernShopping.Lookup.Queries
+{
+    public class ProductByIdQuery
+    {
+        private readonly IMongoCollection<Product> _productCollection;
+        private readonly ILogger<ProductByIdQuery> _logger;
+
+        public ProductByIdQuery(IMongoCollection<Product> productCollection, ILogger<ProductByIdQuery> logger)
+        {
+            _productCollection = productCollection;
+            _logger = logger;
+        }
+
+        public Task<ProductQueryResult> Execute(string productId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ProductQueryResult
+    {
+        public ProductQueryResult(Product product)
+        {
+            Product = product;
+        }
+
+        public Product Product { get; }
+    }
+}
